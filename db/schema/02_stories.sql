1c690b369ab779260f3d85bee2245e1d63afb3a2
Kenniku Tolato <kenniku_you@ymail.com>
2023-10-13 06:09:48 +0000

    Complete db query data types.

--- db/schema/02_stories.sql
+++ db/schema/02_stories.sql
@@ -2,9 +2,10 @@ DROP TABLE IF EXISTS stories CASCADE;
 
 CREATE TABLE stories (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INT NOT NULL,
+  user_id INTEGER NOT references users ON DELETE CASCADE,
   main_story TEXT NOT NULL,
   title VARCHAR(255) ,
-  story_status boolean,
-  date_created timestamp
+  story_status BOOLEAN,
+  date_created TIMESTAMP,
+  date_completed DATE
 );
