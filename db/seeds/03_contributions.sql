e2483f8acdf33b9ffc18298104e838ae36133430
Kenniku Tolato <kenniku_you@ymail.com>
2023-10-13 06:25:42 +0000

    Insert initial seed for contributions.

--- db/seeds/03_contributions.sql
+++ db/seeds/03_contributions.sql
@@ -1,3 +1,17 @@
 -- Users table seeds here
-INSERT INTO contributions (id, story_id, votes, story_proposal, date_created, proposal_status)
-VALUES ();
+INSERT INTO contributions
+  (
+    story_id,
+    votes,
+    story_proposal,
+    date_created,
+    proposal_status
+  )
+VALUES
+  (
+    1,
+    0,
+    'There was a dragon within the dungeon hoarding gold and always sleeping.',
+    Now(), -- can also be set as default within the schema create
+    false, -- false: meaning not accepted by default? true: when accepted to story
+  );
