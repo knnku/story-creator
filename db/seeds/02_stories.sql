43c8f29e86ccf270d2b9c470aef332d0194ed3d6
linkwithlionhart <linkwithlionhart@gmail.com>
2023-10-13 20:59:23 -0700

    Create seed data for stories

--- db/seeds/02_stories.sql
+++ db/seeds/02_stories.sql
@@ -1,3 +1,7 @@
 -- Users table seeds here
+-- Users table seeds should have been executed before this
 INSERT INTO stories (user_id, main_story, title, story_status, date_created)
-VALUES ()
+VALUES
+(1, 'Once upon a time, in a land far, far away...', 'The Distant Kingdom', TRUE, '2023-01-01 10:00:00'),
+(2, 'In a bustling city, a young artist named Mia...', 'Dreams of the Canvas', FALSE, '2023-02-15 14:30:00'),
+(3, 'Amidst the endless cosmos, a spaceship...', 'Voyage to the Unknown', FALSE, '2023-05-10 18:20:00');
