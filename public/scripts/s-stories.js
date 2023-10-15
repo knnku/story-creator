e9cbfd0142ea47b530664a3d2abba2459aea98a1
linkwithlionhart <linkwithlionhart@gmail.com>
2023-10-14 17:25:31 -0700

    Refactor code, fix bugs

--- public/scripts/s-stories.js
+++ public/scripts/s-stories.js
@@ -2,38 +2,39 @@
 // if neither admin or guest, stories are still shown by default
 
 $(document).ready(function(){
-  // if user is admin and clicks login then show story list
-  // show the main viewer
-  $('#login').on('click', (event) => {
-    event.preventDefault();
+  // Initially show stories and main viewer (irrespective of login status)
+  $('#story-list').show();
+  $('#main-viewer').show();
 
-    const $userInput = $('#login-form').serialize();
+ $('#login').on('click', (event) => {
+   event.preventDefault();
+   const $userInput = $('#login-form').serialize();
 
-    console.log('Login button clicked!');
-    $.ajax({
-      method: 'POST',
-      url: 'users/login',
-      data: $userInput
-    })
-    .done(response => {
-      console.log(res);
-      // After login success, display story and main viewer.
-      if (response.success) {
-        $('#story-list').show();
-        $('#main-viewer').show();
+   console.log('Login button clicked!');
 
-        // Check if user is 'admin'.
-        if (response.userType === 'admin') {
-          $('#add-story').show;
-        } else if(response.userType === 'guest') {
-          $('#add-story').hide;
-        } else {
-          console.log(response.message || 'Error logging in.');
-        }
-        }
-    })
-    .catch(err => {
-      console.log(err.message);
-    });
-  });
+   $.ajax({
+     method: 'POST',
+     url: 'users/login',
+     data: $userInput
+   })
+   .done(response => {
+     console.log(response);
+     // After login success, check for user type.
+     if (response.success) {
+
+       // If user is 'admin', show button.
+       if (response.userType === 'admin') {
+         $('#add-story').show;
+       // If user is not 'admin', hide button.
+       } else if(response.userType !== 'guest') {
+         $('#add-story').hide;
+       } else {
+         console.log(response.message || 'Error logging in.');
+       }
+     }
+   })
+   .catch(err => {
+     console.log(err.message);
+   });
+ });
 });
