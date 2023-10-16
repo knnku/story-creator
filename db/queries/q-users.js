70901442ebe5d058b7c0ce295a2a0374959d34d2
linkwithlionhart <linkwithlionhart@gmail.com>
2023-10-16 13:26:10 -0700

    Return values from newly added story to db

--- db/queries/q-users.js
+++ db/queries/q-users.js
@@ -11,4 +11,31 @@ const getUser = (username) => {
     });
 };
 
+// Return values from add story post to db.
+const insertStory = storyValues => {
+  const query = `
+    INSERT INTO stories (
+      user_id,
+      main_story,
+      title,
+      story_status,
+      date_created
+    ) VALUES ($1, $2, $3, $4, $5) RETURNING *
+  `;
+
+  const values = [
+    storyValues.user_id,
+    storyValues.main_story,
+    storyValues.title,
+    storyValues.story_status,
+    storyValues.date_created
+  ];
+
+  return db.query(query, values)
+    .then(data => {
+      // Return newly added story.
+      return data.rows[0];
+    });
+};
+
 module.exports = { getUser };
