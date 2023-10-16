9f7e204e81e66eaa8392c49fbf8a788ce449eabf
linkwithlionhart <linkwithlionhart@gmail.com>
2023-10-16 11:59:30 -0700

    Implement scripting logic for admin to add new story

--- routes/r-stories.js
+++ routes/r-stories.js
@@ -6,9 +6,10 @@
  */
 
 const express = require('express');
-const { getStories } = require('../db/queries/q-stories');
+const { getStories, insertStory } = require('../db/queries/q-stories');
 const router  = express.Router();
 
+// Endpoint to fetch stories from db.
 router.get('/stories', (req, res) => {
   getStories()
     .then(stories => {
@@ -18,6 +19,29 @@ router.get('/stories', (req, res) => {
       console.error("Error fetching stories:", err);
       // Catch the internal server error.
       res.status(500).json({ error: "Internal server error "});
+    })
+});
+
+// Endpoint to add new story for admin.
+router.post('/stories', (req, res) => {
+  const { user_id, main_story, title } = req.body;
+  const story_status = false; // Not yet complete as set by admin.
+  const date_created = new Date();
+  const storyValues = {
+    user_id,
+    main_story,
+    title,
+    story_status,
+    date_created
+  };
+
+  insertStory(storyValues)
+    .then (response => {
+      res.json({ message: "Your story has now begun!", data: response });
+    })
+    .catch(err => {
+      console.error("Error adding story:", err);
+      res.status(500).json({ error: "Internal server error."});
     })
 });
 
