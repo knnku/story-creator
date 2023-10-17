217bd8e39d1a4824e60292cd9a551c3cdd606432
linkwithlionhart <linkwithlionhart@gmail.com>
2023-10-16 22:56:41 -0700

    Reimplement feature for add story button to appear when admin logs in

--- public/scripts/s-users.js
+++ public/scripts/s-users.js
@@ -1,13 +1,21 @@
 $(document).ready(function () {
   const usrCookie = document.cookie;
-  const userWho = usrCookie.split("="); //bruteforce cookie params to get admin
+  const userWho = usrCookie.split("="); // Brute force cookie params to get admin.
 
+  // If a user is already logged in, display a welcome message and handle "admin" functionalities.
   if (userWho[1] === "admin" || userWho[1] === "guest") {
-    $("#login-form").removeClass("d-flex").hide(); //Remove 'd-flex class' becase it overrides hide using !important
+    $("#login-form").removeClass("d-flex").hide(); // Remove 'd-flex class' because it overrides hide using !important
     $("#login-message span").text(`Welcome, ${userWho[1]}`);
     $("#login-message").show();
+
+    // WL new - Show add story button for admins.
+    if (userWho[1] === "admin") {
+      $("#add-story").show();
+    }
+    // ----- fin
   }
 
+  // Event handler for login.
   $("#login").on("click", (event) => {
     event.preventDefault();
     const $userInput = $("#login-form").serialize();
@@ -18,11 +26,18 @@ $(document).ready(function () {
       data: $userInput
     })
       .done((response) => {
-
         if (response.success) {
           $("#login-form").removeClass("d-flex").hide();
           $("#login-message span").text(response.message).show();
           $("#login-message").show();
+
+          // WL new - Check if the user type is admin and show the "Add Story" button.
+          const userType = response.data[0].username;
+          if (userType === "admin") {
+            $("#add-story").show();
+          }
+          // ----- fin
+
         } else {
           $("#login-message span")
             .text(response.message || "Error logging in.")
@@ -36,10 +51,11 @@ $(document).ready(function () {
 
   console.log('DOM ready!');
 
+  // Event handler for logout.
   $("#logout").on("click", (event) => {
     event.preventDefault();
 
-    //hide all admin stuff and set cookie to null
+    // Hide all admin stuff and set cookie to null.
     $("#add-story").hide();
     $("#login-message span").hide();
     $("#login-message").hide();
