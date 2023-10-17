898c69b3b5dca756d71c044092b8072ab6502a50
linkwithlionhart <linkwithlionhart@gmail.com>
2023-10-16 22:43:16 -0700

    Clean, refactor, and improve add story script

--- public/scripts/s-add-story.js
+++ public/scripts/s-add-story.js
@@ -1,8 +1,9 @@
 $(document).ready(() => {
 
+  // Function that returns an HTML structure for a story creation form.
   const $newStoryForm = () => {
     const storyForm = `
-      <div class="story-creation-form">
+      <div class="story-creation-form" style="display:none;">
         <input type="text" id="new-story-title" placeholder="Enter your title.">
         <textarea id="new-story-text" placeholder="Begin your story!"></textarea>
         <button id="submit-story">CREATE</button>
@@ -12,9 +13,28 @@ $(document).ready(() => {
     return storyForm;
   }
 
+  // Event listener for the 'add-story' button.
   $('#add-story').on('click', () => {
     $("#story-view").empty();
+
+    // If the form doesn't already exist in the DOM, append it. Otherwise, just show it.
+    if (!$(".story-creation-form").length) {
+        $("#story-view").append($newStoryForm());
+    }
     $(".story-creation-form").show();
-  })
+  });
+
+  // Event listener to hide the form when 'Cancel' is clicked.
+  $(document).on('click', '#cancel-story', () => {
+    $(".story-creation-form").hide();
+  });
+
+  // You can also add listeners for the form submission here if desired.
+  // Example:
+  /*
+  $(document).on('click', '#submit-story', () => {
+    // Logic to handle form submission.
+  });
+  */
 
 });
