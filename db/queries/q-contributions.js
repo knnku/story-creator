33a2a41a6e8d8257db2501544b0edf459e361c04
linkwithlionhart <linkwithlionhart@gmail.com>
2023-10-18 20:05:30 -0700

    Implement add contribution query

--- db/queries/q-contributions.js
+++ db/queries/q-contributions.js
@@ -1,5 +1,35 @@
 const db = require('../connection');
 
+const addContribution = (contributionData) => {
+  const query = `
+    INSERT INTO contributions (
+      story_id,
+      user_id,
+      votes,
+      story_proposal,
+      date_created,
+      proposal_status
+    ) VALUES ($1, $2, $3, $4, $5, $6)
+    RETURNING *;
+  `;
+
+  const values = [
+    contributionData.story_id, // ID of the story the contribution belongs to
+    contributionData.user_id || null, // ID of the user (can be null for anonymous)
+    0, // Initial votes always start at 0
+    contributionData.story_proposal, // The proposed contribution text
+    new Date(), // Current date and time
+    false // Initial proposal status is always false (not yet approved)
+  ];
+
+  return db.query(query, values)
+    .then(response => response.rows[0]) // Return the newly added contribution
+    .catch(err => {
+      console.error("Error adding contribution:", err);
+      throw err; // Propagate the error so it can be handled by the caller
+    });
+};
+
 const upvoteContribution = (contribution_id) => {
   const query = `
     UPDATE contributions
