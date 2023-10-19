b2e2dbad2235e19052cc54c0d938b1e2d1942826
linkwithlionhart <linkwithlionhart@gmail.com>
2023-10-18 19:58:24 -0700

    Update contribution queries

--- db/queries/q-contributions.js
+++ db/queries/q-contributions.js
@@ -1,59 +1,83 @@
-/**
- * Assumptions for team at this point:
- * 1. Each story has a unique ID.
- * 2. When contributions are added, they're appended to the end of the main_story with two newline breaks.
- * 3. The `story_id` in `insertStory` is assumed to be unique and either generated externally or input manually.
- * 4. Contributions are simple text additions to the main story without tracking individual contributors.
- */
-
 const db = require('../connection');
 
-const getStories = () => {
-  const query = 'SELECT * FROM stories';
-  return db.query(query)
-    .then(data => {
-      return data.rows;
-    });
-};
-
-
-const addContributionToStory = (storyId, contribution) => {
+const upvoteContribution = (contribution_id) => {
   const query = `
-    UPDATE stories
-    SET main_story = CONCAT(main_story, '\n\n', $2)
+    UPDATE contributions
+    SET votes = votes + 1
     WHERE id = $1
+    RETURNING *;
   `;
 
-  const values = [storyId, contribution];
+  const values = [contribution_id];
 
   return db.query(query, values)
-    .then(data => {
-      return data.rows;
-    });
+    .then(data => data.rows[0])
+    .catch(err => console.error("Error upvoting contribution:", err));
 };
 
-const insertStory = (storyValues) => {
+const approveContribution = (contribution_id) => {
   const query = `
-    INSERT INTO stories (
-      story_id,
-      votes,
-      story_proposal,
-      date_created,
-      proposal_status
-    ) VALUES ($1, $2, $3, $4, $5)`;
+    UPDATE contributions
+    SET proposal_status = true
+    WHERE id = $1
+    RETURNING *;
+  `;
 
-    const values = [
-    storyValues.story_id,
-    storyValues.votes,
-    storyValues.story_proposal,
-    storyValues.date_created,
-    storyValues.proposal_status
-  ];
+  const values = [contribution_id];
 
   return db.query(query, values)
-    .then(data => {
-      return data.rows;
-    });
+    .then(data => data.rows[0])
+    .catch(err => console.error("Error approving contribution:", err));
 };
 
-module.exports = { getStories, getStoryById, addContributionToStory, insertStory };
+module.exports = { addContribution, upvoteContribution, approveContribution };
+
+// Junk code:
+
+// const getStories = () => {
+//   const query = 'SELECT * FROM stories';
+//   return db.query(query)
+//     .then(data => {
+//       return data.rows;
+//     });
+// };
+
+
+// const addContributionToStory = (storyId, contribution) => {
+//   const query = `
+//     UPDATE stories
+//     SET main_story = CONCAT(main_story, '\n\n', $2)
+//     WHERE id = $1
+//   `;
+
+//   const values = [storyId, contribution];
+
+//   return db.query(query, values)
+//     .then(data => {
+//       return data.rows;
+//     });
+// };
+
+// const insertStory = (storyValues) => {
+//   const query = `
+//     INSERT INTO stories (
+//       story_id,
+//       votes,
+//       story_proposal,
+//       date_created,
+//       proposal_status
+//     ) VALUES ($1, $2, $3, $4, $5)`;
+
+//     const values = [
+//     storyValues.story_id,
+//     storyValues.votes,
+//     storyValues.story_proposal,
+//     storyValues.date_created,
+//     storyValues.proposal_status
+//   ];
+
+//   return db.query(query, values)
+//     .then(data => {
+//       return data.rows;
+//     });
+// };
