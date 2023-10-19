7ba88c64c88ae5768362dc5b6b07746268b05512
linkwithlionhart <linkwithlionhart@gmail.com>
2023-10-18 20:21:05 -0700

    Reimplement contribution script

--- public/scripts/s-contributions.js
+++ public/scripts/s-contributions.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
 
+  const genContributionView = (storyProposal, contributionId, isAdmin) => {
+    return `
+    <div class="contribution" data-id="${contributionId}">
+      <p>${storyProposal}</p>
+      <button class="upvote-btn">Upvote</button>
+      ${isAdmin ? '<button class="approve-btn">Approve</button>' : ''}
+    </div>`;
+  };
+
   $('#submit-contribution').on('click', () => {
     const storyProposal = $('#exampleFormControlTextarea1').val();
     const storyId = $("#story-id").val();  // Assuming you have a hidden input with this id holding the story's id
@@ -14,13 +23,8 @@ $(document).ready(function () {
     })
     .done((response) => {
       if (response.success) {
-        // Append new contribution below the form.
-        const newContribution = `
-        <div class="contribution" data-id="${response.contribution.id}">
-          <p>${storyProposal}</p>
-          <button class="upvote-btn">Upvote</button>
-          ${response.isAdmin ? '<button class="approve-btn">Approve</button>' : ''}
-        </div>`;
+        // Generate the contribution view and append below the form.
+        const newContribution = genContributionView(storyProposal, response.contribution.id, response.isAdmin);
         $(".contributions-list").append(newContribution);
       } else {
         alert(response.message);
@@ -54,103 +58,6 @@ $(document).ready(function () {
     .fail((error) => {
       alert("There was an error approving the contribution. Please try again.");
     });
+  });
+
 });
-
-
-});
-
-  // // IMPORTANT: Renders stories.
-  // const createStoryElement = (story) => {
-  //   const {
-  //     id,
-  //     user_id,
-  //     main_story,
-  //     title,
-  //     story_status,
-  //     date_created,
-  //     date_completed,
-  //   } = story;
-
-  //   let contributeButton = '';
-
-  //   // If story_status is false (meaning incomplete)
-  //   if (!story_status) {
-  //       contributeButton = `
-  //       <button class="contribute-to-story" data-story-id="${id}">Contribute</button>
-
-  //       <!-- Contribution form within the story -->
-  //       <form class="contribution-form" style="display:none;">
-  //           <textarea class="contribution-text" placeholder="Add your part to this story."></textarea>
-  //           <input type="submit" value="Submit">
-  //           <button type="button" class="cancel-contribution">Cancel</button>
-  //       </form>
-  //       `;
-  //   }
-
-  //   const $storyBuild = `
-  //   <article>
-  //     <header>
-  //       <h3>Created by user: #${user_id}</h2>
-  //       <h3>${title}</h2>
-  //     </header>
-  //     <p>${main_story}</p>
-  //     <footer>
-  //       <div>Story completion: ${story_status}</div>
-  //       <div>Date created: ${date_created}</div>
-  //       <div>Date completed: ${date_completed}</div>
-  //       ${contributeButton}
-  //     </footer>
-  //   </article>
-  //   `;
-
-  //   return $storyBuild;
-  // };
-
-  // // Fetch and display stories from the database to append contribution buttons.
-  // $.ajax({
-  //   method: "GET",
-  //   url: "/stories",
-  // })
-  //   .done((stories) => {
-  //     stories.forEach((story) => {
-  //       const $storyElement = createStoryElement(story);
-  //       $(".story-list").append($storyElement);
-  //     });
-  //   })
-  //   .catch((err) => {
-  //     console.log("Error fetching stories for contributions:", err);
-  //   });
-
-  // // Event handler for showing the contribution modal
-  // $(".story-list").on("click", ".contribute-to-story", function() {
-  //   // Toggle the form's visibility.
-  //   $(this).siblings('.contribution-form').toggle();
-  // });
-
-  // // Event handler for cancelling contribution.
-  // $(".story-list").on("click", ".cancel-contribution", function() {
-  //   // Hide the form.
-  //   $(this).closest('.contribution-form').hide();
-  // });
-
-  // // Event handler for submitting a contribution.
-  // $(".story-list").on("submit", ".contribution-form", function(event) {
-  //   event.preventDefault();
-
-  //   const storyId = $(this).siblings(".contribute-to-story").data("story-id");
-  //   const contributionText = $(this).find(".contribution-text").val();
-
-  //   // Make an AJAX POST request to send the contribution to the server.
-  //   $.ajax({
-  //       method: "POST",
-  //       url: `/stories/${storyId}/contribute`,
-  //       data: { contribution: contributionText }
-  //   })
-  //   .done(response => {
-  //       alert(response.message);
-  //       $(this).hide();
-  //   })
-  //   .catch(err => {
-  //       console.log("Error contributing to the story:", err);
-  //   });
-  // });
